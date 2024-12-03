20e423fc5c3990ceb701f347619361a4217f1f4d
Dan Miltenberger <146145186+danmiltenberger@users.noreply.github.com>
2024-12-03 13:18:54 -0500

    rtc clock serial, not variable

---
+++ electropeak_rtc/electropeak_rtc.ino
@@ -0,0 +1,45 @@
+   /*  
+modified on Nov 22, 2020
+Modified by MehranMaleki from Arduino Examples
+Home
+*/
+
+#include <Wire.h>
+#include <ds3231.h>
+ 
+struct ts t; 
+ 
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  DS3231_init(DS3231_CONTROL_INTCN);
+  /*----------------------------------------------------------------------------
+  In order to synchronise your clock module, insert timetable values below !
+  ----------------------------------------------------------------------------*/
+  t.hour=13; 
+  t.min=14;
+  t.sec=0;
+  t.mday=3;
+  t.mon=12;
+  t.year=2024;
+ 
+  DS3231_set(t); 
+}
+
+void loop() {
+  DS3231_get(&t);
+  Serial.print("Date : ");
+  Serial.print(t.mday);
+  Serial.print("/");
+  Serial.print(t.mon);
+  Serial.print("/");
+  Serial.print(t.year);
+  Serial.print("\t Hour : ");
+  Serial.print(t.hour);
+  Serial.print(":");
+  Serial.print(t.min);
+  Serial.print(".");
+  Serial.println(t.sec);
+
+  delay(1000);
+}
---
+++ practice_rtc/practice_rtc.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include "DS3231.h"
+
+DS3231 rtc;
+DateTime dt;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(); // Initialize I2C communication
+  rtc.begin();  // Initialize the RTC module
+  
+  // Uncomment the line below to set the RTC to the date and time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+void loop() {
+  displayDateTime();
+  delay(1000);
+}
+
+void displayDateTime() {
+  dt = rtc.now();
+  
+  Serial.print("Date: ");
+  Serial.print(dt.year(), DEC);
+  Serial.print('/');
+  Serial.print(dt.month(), DEC);
+  Serial.print('/');
+  Serial.print(dt.day(), DEC);
+  
+  Serial.print("  Time: ");
+  Serial.print(dt.hour(), DEC);
+  Serial.print(':');
+  Serial.print(dt.minute(), DEC);
+  Serial.print(':');
+  Serial.println(dt.second(), DEC);
+}
